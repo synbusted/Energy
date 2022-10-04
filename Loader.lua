17717a3ae949f7b413c31f3b9a95d7521617f924
VexusHub <91941885+vrtrz@users.noreply.github.com>
2022-10-04 21:04:39 +0700

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,9 +0,0 @@
-local games = {
-    [{8750997647}] = "https://raw.githubusercontent.com/vrtrz/VexusHub/main/TappingLegendsX.lua",
-}
-
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); break
-    end
-end
